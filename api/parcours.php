5c1fe9a09d70c6c639b820a7a963005160aec3db
Marine Bernard <bernard.marine62330@gmail.com>
2026-08-03 15:33:03 +0200

    feat : page parcours affichage dynamique API

--- api/parcours.php
+++ api/parcours.php
@@ -9,7 +9,7 @@ try {
     'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
     DB_USER,
     DB_PASS,
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
   );
 } catch (PDOException $e) {
   http_response_code(500);
